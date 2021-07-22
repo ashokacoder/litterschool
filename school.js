489dcf695d325b03a5816ac3b2cf4f42e0afed8e
ashokacoder <86926629+ashokacoder@users.noreply.github.com>
2021-07-22 13:35:26 -0400

    my second site

---
+++ school.js
@@ -0,0 +1,19 @@
+
+
+const closetogle = document.getElementById('closetogle');
+const navabaar = document.getElementById('navabaar');
+const togglea = document.getElementById('togglea');
+
+
+togglea.addEventListener('click', ()=>{
+    navabaar.classList.toggle('active')
+});
+
+closetogle.addEventListener('click', ()=>{
+    navabaar.classList.toggle('active')
+});
+
+
+
+
+
